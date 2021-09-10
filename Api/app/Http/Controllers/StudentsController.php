0d434dbc94eee3c46aabf5e22c556c38827dc480
Savoae Alex <a.savoae@software-dep.net>
2021-09-10 09:03:43 +0300

    create student apliance

---
+++ Api/app/Http/Controllers/StudentsController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use  App\Models\Student;
+use Illuminate\Support\Facades\Validator;
+
+class StudentsController extends Controller
+{
+    public function index() {
+        return response()->json(Student::all());
+    }
+
+    public function edit($user_id){
+
+        $student = Student::find($user_id);
+        if($student) return response()->json(['success' => true, 'result' =>  $student], 200);
+        else return response()->json(['success' => false, 'result' => 'Company not found!'], 400);
+    }
+
+    public function store(Request $request){
+      //dd($request);
+        $validator = Validator::make($request->all(), [
+            'first_name' => 'required',
+            'last_name' => 'required',
+
+        ]);
+
+        if ($validator->fails()) return response()->json(['success' => false, 'result' => $validator->errors()->first()], 404);
+        $student = new Student($request->all());
+        $documents = [];
+        if($request->files){
+            foreach($request->files as $file){
+
+
+                $original_filename = $file->getClientOriginalName();
+                $original_filename_arr = explode('.', $original_filename);
+                $file_ext = end($original_filename_arr);
+                $destination_path = './upload/students/' . date('Y-m-d') . '/documents';
+                $image = $original_filename;
+                $file->move($destination_path, $image);
+                $documents[] =  $destination_path.'/'.$image;
+            }
+            $student->documents = $documents;
+        }
+
+
+
+
+        $student->save();
+
+
+        if($student) return response()->json(['success' => true, 'result' => $student], 200);
+        else return response()->json(['success' => false, 'result' => 'Save error.'], 400);
+    }
+
+    public function delete($user_id){
+
+        $student = Student::find($user_id);
+
+        if($student) {
+            $username = $student->first_name.' '.$student->last_name;
+            $student->delete();
+            return response()->json(['success' => true, 'result' => ["name" => $username]], 200);
+        } else return response()->json(['success' => false, 'result' => 'Student not found.'], 400);
+    }
+
+    public function update($user_id, Request $request){
+
+        $validator = Validator::make($request->all(), [
+            'first_name' => 'required',
+            'last_name' => 'required',
+
+        ]);
+
+        if ($validator->fails()) return response()->json(['success' => false, 'result' => $validator->errors()->first()], 404);
+
+        $student = Student::find($user_id);
+        $student->fill($request->all());
+        $documents = [];
+        if($request->files){
+            foreach($request->files as $file){
+
+
+                $original_filename = $file->getClientOriginalName();
+                $original_filename_arr = explode('.', $original_filename);
+                $file_ext = end($original_filename_arr);
+                $destination_path = './upload/students/' . date('Y-m-d') . '/documents';
+                $image = $original_filename;
+                $file->move($destination_path, $image);
+                $documents[] =  $destination_path.'/'.$image;
+            }
+            $student->documents = $documents;
+        }
+
+
+
+
+        $student->save();
+
+
+
+        if($student) return response()->json(['success' => true, 'result' => $student], 200);
+        else return response()->json(['success' => false, 'result' => 'Save error.'], 400);
+
+    }
+}
--- Api/routes/web.php
+++ Api/routes/web.php
@@ -7,12 +7,22 @@ $router->group(['middleware' => 'auth', 'prefix' => 'api'], function ($router) {
     //users routes
     $router->group(['prefix' => 'users'], function ($router) {
         $router->get('/', 'AuthController@index');
-        $router->get('create', 'UsersController@create');
-        $router->post('/', 'UsersController@store');
-        $router->get('{user_id}', 'UsersController@show');
-        $router->get('{user_id}/edit', 'UsersController@edit');
-        $router->put('{user_id}', 'UsersController@update');
-        $router->delete('{user_id}', 'UsersController@delete');
+        $router->post('/', 'UserController@store');
+        $router->get('{user_id}', 'UserController@edit');
+        $router->get('{user_id}/edit', 'UserController@edit');
+        $router->put('{user_id}', 'UserController@update');
+        $router->delete('{user_id}', 'UserController@delete');
+    });
+
+    //students route
+    $router->group(['prefix' => 'students'], function ($router) {
+        $router->get('/', 'StudentsController@index');
+        $router->get('create', 'StudentsController@create');
+        $router->post('/', 'StudentsController@store');
+        $router->get('{user_id}', 'StudentsController@edit');
+        $router->get('{user_id}/edit', 'StudentsController@edit');
+        $router->put('{user_id}', 'StudentsController@update');
+        $router->delete('{user_id}', 'StudentsController@delete');
     });
 
 
